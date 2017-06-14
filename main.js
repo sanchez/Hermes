00b512b9836c15d2fc299fa0644595522931e22f
Daniel Fitz <dfitz360@gmail.com>
2017-06-14 23:52:50 +1000

    Added handling for headers, really really basic

--- main.js
+++ main.js
@@ -1,16 +1,49 @@
 #!/usr/bin/env node
 
+var chalk = require('chalk');
+console.log(chalk.blue("Running Hermes - Markdown to pdf"));
+var PDFDocument = require('pdfkit');
+var doc = new PDFDocument({bufferPages: true});
+var handler = require("./lib/doc.js");
+
 function check_arguments(args) {
-    if (args.length == 1) {
+    if (args.length != 2) {
         console.log(chalk.bold.red("Invalid arguments length"));
         process.exit(1);
     }
-    return args
+    
+    var fs = require("fs");
+    var contents = fs.readFileSync(args[0], 'utf8');
+    doc.pipe(fs.createWriteStream(args[1]));
+    return contents;
 }
 
-var chalk = require('chalk');
-console.log(chalk.blue("Running Hermes - Markdown to pdf"));
-global.args = check_arguments(process.argv.slice(2));
+function process_header(line) {
+    var regex = /^(#{1,3})(\{(.+)\})? (.+)$/;
+    var result = line.match(regex);
+    if (result == null) {
+        return false;
+    }
+    var headingLevel = result[1];
+    var headingText = result[4];
+    var headingOptions = result[3];
+    handler.heading(doc, headingLevel, headingText, headingOptions);
+    return true;
+}
 
+function process_plain_text(line) {
+    line = line.replace(/\\#/g, "#");
+    console.log("Text: " + line);
+    return true;
+}
 
-console.log(global.args);
+var contents = check_arguments(process.argv.slice(2));
+contents.split("\n").forEach((line) => {
+    if (process_header(line)) {
+    } else {
+        process_plain_text(line);
+    }
+});
+
+doc.flushPages();
+doc.end();
--- test.md
+++ test.md
@@ -1,1 +1,2 @@
+# Header
 Hello World
